a3499e4f094d8b080d655525afafe66d6b1b5225
donaldianr-design <donaldianr@gmail.com>
2026-03-12 13:31:49 +0000

    Force IPv4 for Supabase connection

--- api/server.js
+++ api/server.js
@@ -18,9 +18,8 @@ console.log("DATABASE_URL:", process.env.DATABASE_URL);
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: { rejectUnauthorized: false },
+  family: 4
 });
 
 // -----------------------------
